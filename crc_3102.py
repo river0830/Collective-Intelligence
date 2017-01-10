90d890f97976cb76bcadd968da7ad0c5acd64810
river <binghan0351@qq.com>
2017-01-10 23:18:37 +0800

    1. crc_3102.py add;

--- crc_3102.py
+++ crc_3102.py
@@ -6,7 +6,7 @@ import ttk
 
 import river
 
-class popmemu(object):
+class Popmemu(object):
 	def __init__(self, root, entry):
 		self.root  = root
 		self.entry = entry
@@ -16,16 +16,57 @@ class popmemu(object):
 		self.txt = self.entry.get()
 
 
-class crc16(object):
+class Crc16(object):
 	pass
 
-class gui(tk.Frame):
+class Gui(tk.Frame):
 	def __init__(self, master = None):
 		tk.Frame.__init__(self, master)
 
-		self.master = master
-		self.create_widget()
-		self.create_popmenu()
+		master.title("River's crc16 calculator")
+		self.create_widget(master)
+		self.create_popmenu(master)
 
-	def create_widget(self):
-		self.
+	def create_widget(self, master):
+		self.show = tk.StringVar()
+		self.entry = tk.Entry(master, textvariable = self.show)\
+					.pack(expand=tk.YES, side=tk.TOP, fill=tk.BOTH)
+		#16 key
+		for key in ('01234567', '89ABCDEF'):
+			fram = self.create_frame()
+			for c in key:
+				self.create_buttons(fram, c, lambda x=self.show, c=c: x.set(x.get()+c))
+
+		fram3 = self.create_frame()
+		self.create_buttons(fram3, 'Blank',
+							lambda x=self.show: x.set(x.get()+' '))
+		self.create_buttons(fram3, 'Clear',
+							lambda x=self.show: x.set(''))
+		self.create_buttons(fram3, 'Backspace',
+							lambda x=self.show: x.set(self.back(x.get())))
+		self.create_buttons(fram3, '=',
+							lambda : self.calc_crc)
+
+	def create_buttons(self, fram, str, commands, widths=6):
+		tk.Button(fram, text=str, width=widths, command=commands)\
+						.pack(expand=tk.YES, side=tk.LEFT, fill=tk.BOTH, padx=1, pady=1)
+
+	def create_frame(self, sides=tk.TOP):
+		fram = tk.Frame(self.master)
+		fram.pack(expand=tk.YES, side=sides, fill=tk.BOTH)
+		return fram
+
+	def back(self, str):
+		if(len(str) > 0):
+			return str[:-1]
+		else:
+			return str
+
+	def create_popmenu(self, master):
+		pass
+
+	def calc_crc(self):
+		pass
+
+if __name__ == '__main__':
+	Gui(tk.Tk()).mainloop()
